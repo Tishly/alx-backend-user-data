f059909826f00ca2fbb47b7f210d3de733244200
Tishly <ekaetteessien27@gmail.com>
2023-03-02 19:21:38 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,11 @@ Regex-ing function
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str, seperator: str):
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
     """
     A function that returns the log message obfuscated
     """
-    return re.sub(f"({'|'.join(fields)})", redaction, message, flags=re.I)
+    return re.sub(f"({'|?'.join(fields)})", redaction, message, flags=re.I)
+    # return re.sub("(" + separator.join(fields) + ")=(.*?)" + separator,
+    # "\\1=" + redaction + separator, message)
