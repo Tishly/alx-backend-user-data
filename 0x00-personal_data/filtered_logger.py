b9e0d97041a94202b41bfd99b3deec5891b65487
Tishly <ekaetteessien27@gmail.com>
2023-03-03 00:10:17 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,6 +13,3 @@ def filter_datum(fields: list, redaction: str,
         message = re.sub(f'{i}=.*?{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
-    # regex = fr'({separator}|^)({"|".join(i)})=.*?({separator}|$)'
-    # return re.sub(regex, f'\g<1>\\2={redaction}\g<3>', message)
-    # return re.sub(fr'({separator}|^)({"|".join(fields)})=([^{separator}]*)({separator}|$)', f'\\1\\2={redaction}\\3\\4', message)
