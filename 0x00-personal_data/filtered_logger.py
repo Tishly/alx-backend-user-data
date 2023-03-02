99d653679d50ff9a50664cddb173a3072a03d2a3
Tishly <ekaetteessien27@gmail.com>
2023-03-03 00:15:36 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,11 @@ Regex-ing function
 """
 
 import re
+import logging
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ A function that returns the log message obfuscated """
     for i in fields:
