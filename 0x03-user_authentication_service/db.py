ec94d9b01545d3a04777f912d97ae9abf6540610
Tishly <ekaetteessien27@gmail.com>
2023-03-18 02:39:11 +0100

    Find users by keys

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,4 +39,23 @@ class DB:
         self._session.add(user)
         self._session.commit()
         self._session.refresh(user)
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds users based on input
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
         return user
